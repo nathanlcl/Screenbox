31377c6bfd83a97b803069e0f6fff5662653177c
nathanlcl <121922177+nathanlcl@users.noreply.github.com>
2026-08-23 03:55:55 +0800

    placeholder

--- Screenbox/App.xaml.cs
+++ Screenbox/App.xaml.cs
@@ -1,344 +1,44 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security;
+using System.IO;
+using System.Text.Json;
 using System.Threading.Tasks;
-using CommunityToolkit.Mvvm.Messaging;
-using CommunityToolkit.WinUI.Helpers;
-using LibVLCSharp.Shared;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Screenbox.Core;
-using Screenbox.Core.Helpers;
-using Screenbox.Core.Messages;
-using Screenbox.Core.Services;
-using Screenbox.Core.ViewModels;
-using Screenbox.Helpers;
-using Screenbox.Lively;
-using Screenbox.Pages;
-using Screenbox.Services;
-using Sentry;
-using Sentry.Protocol;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.ApplicationModel.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
 
 namespace Screenbox;
 
-/// <summary>
-/// Provides application-specific behavior to supplement the default Application class.
-/// </summary>
-sealed partial class App : Application
+public sealed partial class App : Application
 {
-    private readonly IServiceProvider _services;
-    private readonly ILogger<App> _logger;
-    private readonly ITelemetryService _telemetryService;
+    private const string Win2DBugHandlingStateFileName = "win2d-bug-handling-state.json";
+    private static string Win2DBugHandlingStateFilePath =>
+        Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, Win2DBugHandlingStateFileName);
 
-    /// <summary>
-    /// Initializes the singleton application object.  This is the first line of authored code
-    /// executed, and as such is the logical equivalent of main() or WinMain().
-    /// </summary>
+    private static bool _suppressGlobalCrashHandling;
+
     public App()
     {
-        // Initialize Sentry SDK and immediately register error handlers before XAML is initialized
-        _ = InitializeSentrySdk();
-        UnhandledException += App_UnhandledException;
-        CoreApplication.UnhandledErrorDetected += CoreApplication_UnhandledErrorDetected;
-
-        InitializeComponent();
-
-        if (DeviceInfoHelper.IsXbox)
-        {
-            // Disable pointer mode on Xbox
-            // https://learn.microsoft.com/en-us/windows/uwp/xbox-apps/how-to-disable-mouse-mode#xaml
-            RequiresPointerMode = ApplicationRequiresPointerMode.WhenRequested;
-
-            // Use Reveal focus for 10-foot experience
-            // https://learn.microsoft.com/en-us/windows/apps/design/input/gamepad-and-remote-interactions#reveal-focus
-            FocusVisualKind = FocusVisualKind.Reveal;
-        }
-
-        // Disable automatic High Contrast adjustments
-        // https://learn.microsoft.com/en-us/windows/apps/design/accessibility/high-contrast-themes#setting-highcontrastadjustment-to-none
-        HighContrastAdjustment = ApplicationHighContrastAdjustment.None;
-
-        _services = ConfigureServices();
-        CommunityToolkit.Mvvm.DependencyInjection.Ioc.Default.ConfigureServices(_services);
-
-        _logger = _services.GetRequiredService<ILogger<App>>();
-        _telemetryService = _services.GetRequiredService<ITelemetryService>();
-
-        Suspending += OnSuspending;
+        this.InitializeComponent();
+        this.UnhandledException += OnUnhandledException;
     }
 
-    [SecurityCritical]
-    private void CoreApplication_UnhandledErrorDetected(object? sender, UnhandledErrorDetectedEventArgs e)
+    private static void OnUnhandledException(object sender, Windows.UI.Xaml.UnhandledExceptionEventArgs e)
     {
-        try
+        if (_suppressGlobalCrashHandling) return;
+        if (IsWin2DBug(e.Exception))
         {
-            e.UnhandledError.Propagate();
-        }
-        catch (Exception ex)
-        {
-            // Tell Sentry this was an unhandled exception
-            ex.Data[Mechanism.HandledKey] = false;
-            ex.Data[Mechanism.MechanismKey] = "CoreApplication.UnhandledErrorDetected";
-
-            // Capture the exception
-            SentrySdk.CaptureException(ex);
-
-            // Flush the event immediately
-            SentrySdk.FlushAsync(TimeSpan.FromSeconds(2)).GetAwaiter().GetResult();
-
-            if (ex is VLCException { Message: "Could not create Direct3D11 device : No compatible adapter found." })
-            {
-                WeakReferenceMessenger.Default.Send(new CriticalErrorMessage(Strings.Resources.CriticalErrorDirect3D11NotAvailable));
-            }
-            else
-            {
-                throw;
-            }
+            // Workaround for Win2D bug with Windows App SDK 1.8: https://github.com/microsoft/Win2D/issues/951
+            e.Handled = true;
+            HandleWin2DBugAsync().Track();
         }
     }
 
-    [SecurityCritical]
-    private void App_UnhandledException(object sender, Windows.UI.Xaml.UnhandledExceptionEventArgs e)
+    private static bool IsWin2DBug(Exception ex)
     {
-        // Get a reference to the exception, because the Exception property is cleared when accessed.
-        var exception = e.Exception;
-        if (exception != null)
-        {
-            // Tell Sentry this was an unhandled exception
-            exception.Data[Mechanism.HandledKey] = false;
-            exception.Data[Mechanism.MechanismKey] = "Application.UnhandledException";
-
-            // Capture the exception
-            SentrySdk.CaptureException(exception);
-
-            // Flush the event immediately
-            SentrySdk.FlushAsync(TimeSpan.FromSeconds(2)).GetAwaiter().GetResult();
-        }
+        return ex is DllNotFoundException or TypeInitializationException or Exception { HResult: -2147467259 };
     }
 
-    private static IServiceProvider ConfigureServices()
+    private static async Task HandleWin2DBugAsync()
     {
-        ServiceCollection services = new();
-        ServiceHelpers.PopulateCoreServices(services);
-        services.AddLivelyWallpaperServices();
-        services.AddSingleton<ITelemetryService, SentryTelemetryService>();
-        services.AddLogging(builder =>
-        {
-#if DEBUG
-            builder.AddDebug();
-            builder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Debug);
-#endif
-            builder.AddSentry(options =>
-            {
-                ConfigureSentryOptions(options);
-                options.InitializeSdk = false;
-                options.MinimumEventLevel = Microsoft.Extensions.Logging.LogLevel.Error;
-                options.MinimumBreadcrumbLevel = Microsoft.Extensions.Logging.LogLevel.Information;
-            });
-        });
-
-        // Services
-        services.AddSingleton<IVlcDialogService, VlcDialogService>();
-        services.AddSingleton<INavigationService, NavigationService>(_ => new NavigationService(
-            new KeyValuePair<Type, Type>(typeof(HomePageViewModel), typeof(HomePage)),
-            new KeyValuePair<Type, Type>(typeof(PlaylistsPageViewModel), typeof(PlaylistsPage)),
-            new KeyValuePair<Type, Type>(typeof(PlaylistDetailsPageViewModel), typeof(PlaylistDetailsPage)),
-            new KeyValuePair<Type, Type>(typeof(VideosPageViewModel), typeof(VideosPage)),
-            new KeyValuePair<Type, Type>(typeof(AllVideosPageViewModel), typeof(AllVideosPage)),
-            new KeyValuePair<Type, Type>(typeof(MusicPageViewModel), typeof(MusicPage)),
-            new KeyValuePair<Type, Type>(typeof(SongsPageViewModel), typeof(SongsPage)),
-            new KeyValuePair<Type, Type>(typeof(ArtistsPageViewModel), typeof(ArtistsPage)),
-            new KeyValuePair<Type, Type>(typeof(AlbumsPageViewModel), typeof(AlbumsPage)),
-            new KeyValuePair<Type, Type>(typeof(NetworkPageViewModel), typeof(NetworkPage)),
-            new KeyValuePair<Type, Type>(typeof(PlayQueuePageViewModel), typeof(PlayQueuePage)),
-            new KeyValuePair<Type, Type>(typeof(SettingsPageViewModel), typeof(SettingsPage)),
-            new KeyValuePair<Type, Type>(typeof(AlbumDetailsPageViewModel), typeof(AlbumDetailsPage)),
-            new KeyValuePair<Type, Type>(typeof(ArtistDetailsPageViewModel), typeof(ArtistDetailsPage)),
-            new KeyValuePair<Type, Type>(typeof(SearchResultPageViewModel), typeof(SearchResultPage)),
-            new KeyValuePair<Type, Type>(typeof(ArtistSearchResultPageViewModel), typeof(ArtistSearchResultPage)),
-            new KeyValuePair<Type, Type>(typeof(AlbumSearchResultPageViewModel), typeof(AlbumSearchResultPage)),
-            new KeyValuePair<Type, Type>(typeof(SongSearchResultPageViewModel), typeof(SongSearchResultPage)),
-            new KeyValuePair<Type, Type>(typeof(VideoSearchResultPageViewModel), typeof(VideoSearchResultPage)),
-            new KeyValuePair<Type, Type>(typeof(FolderViewPageViewModel), typeof(FolderViewPage)),
-            new KeyValuePair<Type, Type>(typeof(FolderListViewPageViewModel), typeof(FolderListViewPage))
-        ));
-
-        return services.BuildServiceProvider();
-    }
-
-    private static IDisposable InitializeSentrySdk()
-    {
-        var initRef = SentrySdk.Init(options => ConfigureSentryOptions(options));
-
-        SentrySdk.ConfigureScope(scope =>
-        {
-            scope.SetTag("device_family", DeviceInfoHelper.DeviceFamily);
-        });
-
-        return initRef;
-    }
-
-    private static void ConfigureSentryOptions(SentryOptions options)
-    {
-        options.Dsn = Secrets.SentryDsn;
-        options.SampleRate = 1.0f;
-        options.IsGlobalModeEnabled = true;
-        options.AutoSessionTracking = true;
-        options.Release = $"screenbox@{Package.Current.Id.Version.ToFormattedString(3)}";
-    }
-
-    private void SetMinWindowSize()
-    {
-        //var view = ApplicationView.GetForCurrentView();
-        //view.SetPreferredMinSize(new Size(480, 270));
-    }
-
-    protected override void OnFileActivated(FileActivatedEventArgs args)
-    {
-        _logger.LogInformation("File activation started. {PreviousExecutionState}", args.PreviousExecutionState);
-
-        Frame rootFrame = InitRootFrame();
-        if (rootFrame.Content is not MainPage)
-        {
-            rootFrame.Navigate(typeof(MainPage), true);
-        }
-
-        Window.Current.Activate();
-        WeakReferenceMessenger.Default.Send(new PlayFilesMessage(args.Files, args.NeighboringFilesQuery));
-    }
-
-    /// <summary>
-    /// Invoked when the application is launched normally by the end user.  Other entry points
-    /// will be used such as when the application is launched to open a specific file.
-    /// </summary>
-    /// <param name="e">Details about the launch request and process.</param>
-    protected override void OnLaunched(LaunchActivatedEventArgs e)
-    {
-        _logger.LogInformation("App launched. {PrelaunchActivated} {PreviousExecutionState}",
-            e.PrelaunchActivated, e.PreviousExecutionState);
-
-        Frame rootFrame = InitRootFrame();
-        LibVLCSharp.Shared.Core.Initialize();
-        _ = StartupInitAsync();
-
-        if (e.PrelaunchActivated) return;
-        CoreApplication.EnablePrelaunch(true);
-        if (rootFrame.Content == null)
-        {
-            SetMinWindowSize();
-            rootFrame.Navigate(typeof(MainPage));
-        }
-
-        // Ensure the current window is active
-        Window.Current.Activate();
-
-#if DEBUG
-        if (System.Diagnostics.Debugger.IsAttached)
-        {
-            //DebugSettings.EnableFrameRateCounter = true;
-            //DebugSettings.EnableRedrawRegions = true;
-            //DebugSettings.FailFastOnErrors = true;
-            //DebugSettings.IsBindingTracingEnabled = true;
-            //DebugSettings.IsOverdrawHeatMapEnabled = true;
-            //DebugSettings.IsTextPerformanceVisualizationEnabled = true;
-        }
-#endif
-    }
-
-    /// <summary>
-    /// Invoked when Navigation to a certain page fails
-    /// </summary>
-    /// <param name="sender">The Frame which failed navigation</param>
-    /// <param name="e">Details about the navigation failure</param>
-    void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-    {
-        throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-    }
-
-    /// <summary>
-    /// Invoked when application execution is being suspended.  Application state is saved
-    /// without knowing whether the application will be terminated or resumed with the contents
-    /// of memory still intact.
-    /// </summary>
-    /// <param name="sender">The source of the suspend request.</param>
-    /// <param name="e">Details about the suspend request.</param>
-    private async void OnSuspending(object sender, SuspendingEventArgs e)
-    {
-        SuspendingDeferral deferral = e.SuspendingOperation.GetDeferral();
-        try
-        {
-            _logger.LogInformation("Application is suspending.");
-            var tasks = WeakReferenceMessenger.Default.Send<SuspendingMessage>().Responses.ToList();
-            if (_telemetryService.IsEnabled)
-            {
-                tasks.Add(_telemetryService.FlushAsync());
-            }
-
-            await Task.WhenAll(tasks);
-        }
-        catch (Exception)
-        {
-            // pass
-        }
-        finally
-        {
-            deferral.Complete();
-        }
-    }
-
-    private async Task StartupInitAsync()
-    {
-        var dbService = _services.GetRequiredService<IDatabaseService>();
-        try
-        {
-            await dbService.InitializeAsync();
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Database initialization failed at startup.");
-        }
-    }
-
-    private Frame InitRootFrame()
-    {
-        // Do not repeat app initialization when the Window already has content,
-        // just ensure that the window is active
-        if (Window.Current.Content is not Frame rootFrame)
-        {
-            // Create a Frame to act as the navigation context and navigate to the first page
-            rootFrame = new Frame();
-
-            rootFrame.NavigationFailed += OnNavigationFailed;
-
-            // Place the frame in the current Window
-            Window.Current.Content = rootFrame;
-            SetMinWindowSize();
-
-            // Turn off overscan on Xbox
-            // https://learn.microsoft.com/en-us/windows/uwp/xbox-apps/turn-off-overscan
-            if (DeviceInfoHelper.IsXbox)
-            {
-                Windows.UI.ViewManagement.ApplicationView.GetForCurrentView()
-                    .SetDesiredBoundsMode(Windows.UI.ViewManagement.ApplicationViewBoundsMode.UseCoreWindow);
-            }
-
-            // Check for RTL flow direction
-            if (GlobalizationHelper.IsRightToLeftLanguage)
-            {
-                rootFrame.FlowDirection = FlowDirection.RightToLeft;
-            }
-
-            var settings = CommunityToolkit.Mvvm.DependencyInjection.Ioc.Default.GetRequiredService<ISettingsService>();
-            rootFrame.RequestedTheme = settings.Theme.ToElementTheme();
-        }
-
-        return rootFrame;
+        // …
     }
 }
